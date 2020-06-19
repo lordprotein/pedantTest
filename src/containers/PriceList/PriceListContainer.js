793a52d23f7af50761245c2a8034f56223b47eda
lordprotein <gamee.logi@gmail.com>
2020-06-19 11:46:45 +0500

    add redux to PriceListContainer

--- src/containers/PriceList/PriceListContainer.js
+++ src/containers/PriceList/PriceListContainer.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
 import { PriceList } from '../../components/PriceList/PriceList';
 import { priceListConst } from '../../constants/priceListConst';
-import { View } from 'react-native';
+import { connect } from 'react-redux';
+import { selectorsPrice } from '../../selectors/selectors';
+import { bindActionCreators } from 'redux';
+import * as actions from '../../actions/actions';
 
 
 class PriceListContainer extends Component {
 
-    render() {
-        const { list } = this.props;
+    componentDidMount = () => {
+        const { setPriceList } = this.props;
+        setPriceList(priceListConst)
+    }
 
+    render() {
+        const { priceList } = this.props;
+        
         return (
-            <PriceList list={list} />
+            <PriceList list={priceList} />
         );
     }
 }
 
 
-export default PriceListContainer;
+const mapStateToProps = state => {
+    return {
+        priceList: selectorsPrice.getList(state)
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    const { setPriceList } = bindActionCreators(actions, dispatch);
+
+    return { setPriceList }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PriceListContainer);
